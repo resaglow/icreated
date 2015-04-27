4066649d47a3accba8cd5a7e5d3e12363612952d
Artem Lobanov <resaglow@gmail.com>
2015-04-27 10:29:37 +0300

    Add OCMock

---
+++ icreated/Pods/Target Support Files/Pods-icreatedTests-FDTake/Pods-icreatedTests-FDTake-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_icreatedTests_FDTake : NSObject
+@end
+@implementation PodsDummy_Pods_icreatedTests_FDTake
+@end
---
+++ icreated/Pods/Target Support Files/Pods-icreatedTests-OCMock/Pods-icreatedTests-OCMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_icreatedTests_OCMock : NSObject
+@end
+@implementation PodsDummy_Pods_icreatedTests_OCMock
+@end
